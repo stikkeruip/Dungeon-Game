f001ca2e0264dc12414dcbec54a3079e4c1128fe
Uipko Stikker <stikkeruipko@gmail.com>
2021-07-05 13:11:59 +0300

    Project Created

---
+++ Source/Dungeon/DungeonGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DungeonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUNGEON_API ADungeonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
